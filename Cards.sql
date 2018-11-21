a58e9a707165a33dc72d6cf342c2e1c255f7e744
YueGu <gu000033@algonquinlive.come>
2018-11-21 11:11:19 -0500

    DATABASE CHART

---
+++ Cards.sql
@@ -0,0 +1,19 @@
+-- Adminer 4.2.6-dev MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `Cards`;
+CREATE TABLE `Cards` (
+  `ID` int(2) NOT NULL,
+  `NAME` int(50) NOT NULL,
+  `TIME` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `Cards` (`ID`, `NAME`, `TIME`) VALUES
+(0,	0,	0),
+(0,	0,	0);
+
+-- 2018-11-21 16:10:37
