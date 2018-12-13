879b7208395f28bd18329beaeff51a60e12c2cad
YueGu <gu000033@algonquinlive.come>
2018-12-12 20:20:47 -0500

    Change the sql

--- Cards.sql
+++ Cards.sql
@@ -7,13 +7,12 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 DROP TABLE IF EXISTS `Cards`;
 CREATE TABLE `Cards` (
-  `ID` int(2) NOT NULL,
-  `NAME` int(50) NOT NULL,
-  `TIME` int(50) NOT NULL
+  `ID` int(225) NOT NULL COMMENT 'ID',
+  `NAME` int(50) NOT NULL COMMENT 'Name',
+  `TIME` int(50) NOT NULL COMMENT 'Time',
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `Cards` (`ID`, `NAME`, `TIME`) VALUES
-(0,	0,	0),
-(0,	0,	0);
+(0,	0,	'Time');
 
 -- 2018-11-21 16:10:37
